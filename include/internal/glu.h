9a1ed093891430130cac051e554e76e2d94d191a
Gorciu <gorciuyt@gmail.com>
2025-10-16 09:16:16 +0200

    patch - small changes in glu.h

--- include/internal/glu.h
+++ include/internal/glu.h
@@ -1,6 +1,9 @@
 #pragma once
 
 // source: /src/internal/glu.cpp
+// description:
+//  glu replacements cuz glu doesn't work for me
+//    ~ gorciu
 
 extern void perspective(float fovy, float aspect, float znear, float zfar);
 extern void lookAt(float eyeX, float eyeY, float eyeZ,
